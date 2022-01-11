49e37d4bc4bda2a08ce3702bc7962e9e55f1fc51
dominik kainz <dominik.kainz@edu.fh-joanneum.at>
2022-01-11 15:13:30 +0100

    update test file

--- src/test/kotlin/at/fhj/ima/employee/employeemanager/EmployeeManagerApplicationTests.kt
+++ src/test/kotlin/at/fhj/ima/employee/employeemanager/EmployeeManagerApplicationTests.kt
@@ -1,13 +1,13 @@
 package at.fhj.ima.employee.employeemanager
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
+import org.junit.jupiter.api.Assertions
 
-@SpringBootTest
-class EmployeeManagerApplicationTests {
+internal class EmployeeManagerApplicationTests {
 
 	@Test
 	fun contextLoads() {
+
 	}
 
 }
