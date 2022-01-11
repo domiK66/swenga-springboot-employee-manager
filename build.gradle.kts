d9af55649971cb6432dc48a84c168b276a76c818
dominik kainz <dominik.kainz@edu.fh-joanneum.at>
2022-01-11 16:08:33 +0100

    add kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,9 @@ repositories {
 }
 
 dependencies {
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
